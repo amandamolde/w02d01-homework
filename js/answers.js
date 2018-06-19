874ff3fc011bff0ef1752797ecbd345ff589b780
Amanda Molde <amanda.molde@gmail.com>
2018-06-18 19:38:47 -0600

    2nd commit - created some data structures and accessed some data

--- js/answers.js
+++ js/answers.js
@@ -40,3 +40,38 @@ const studentInfo = [
 console.log(studentInfo[1].name);
 console.log(studentInfo[1].location);
 console.log(studentInfo[1].favoriteShows);
+
+
+
+
+
+// TAKE IT EASY
+
+// 1. Make an array that holds all of the colors of the rainbow.
+
+const colorsOfRainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+console.log(colorsOfRainbow);
+
+// 2. Write code that will access "blue" from the rainbow array.
+
+colorsOfRainbow[4];
+
+console.log(colorsOfRainbow[4]);
+
+// 3. Make an object that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype.
+
+const amanda = {
+	favoriteFood: 'tacos',
+	hobby: 'homebrewing',
+	currentTown: 'Highlands Ranch aka unincorporated Douglas County',
+	favoriteDatatype: 'booleans',
+}
+
+console.log(amanda);
+
+// 4. Write code that will access your hobby from the object that you just created. 
+
+amanda.hobby;
+
+console.log(amanda.hobby);
